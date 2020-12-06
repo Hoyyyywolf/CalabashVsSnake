18ab474329d4c3f6568bb0ba8c10fa5daf1e6903
Hoyyyywolf <55869568+Hoyyyywolf@users.noreply.github.com>
2020-12-06 14:38:38 +0800

    initial project

---
+++ src/main/java/nju/zjl/cvs/App.java
@@ -0,0 +1,13 @@
+package nju.zjl.cvs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
